98de6e823d94463c5834b60cf535de06d96d5535
Hasibur Rahman <hasibur.rahman@sazim.io>
2023-08-07 16:44:02 +0600

    Product page update

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,20 +1,37 @@
 import Header from "../components/Header";
 import "./product.css";
-import { Link, useParams } from "react-router-dom";
-import { Center, Container, Image, Text, Box } from "@mantine/core";
+import { useParams } from "react-router-dom";
+import {
+  Center,
+  Container,
+  Image,
+  Text,
+  Box,
+  Space,
+  Rating,
+} from "@mantine/core";
 import { useEffect, useState } from "react";
 
 const Product = () => {
   const params = useParams();
   const [image, setImage] = useState("");
-  // console.log(params);
+  const [desc, setDesc] = useState("");
+  const [category, setCategory] = useState("");
+  const [price, setPrice] = useState("");
+  const [rating, setRating] = useState("");
+
   useEffect(() => {
     let data;
     async function fetchData() {
       const res = await fetch(`https://dummyjson.com/products/${params.id}`);
       data = await res.json();
-      console.log(data.thumbnail);
+      // console.log(data.thumbnail);
+      // console.log(data);
       setImage(data.thumbnail);
+      setDesc(data.description);
+      setCategory(data.category);
+      setPrice(data.price);
+      setRating(data.rating);
     }
     fetchData().catch(console.error);
     // fetch(`https://dummyjson.com/products/${params.id}`)
@@ -43,7 +60,22 @@ const Product = () => {
           </Box>
         </Container>
         <Box className="product-info">
-          <Text></Text>
+          <Text fz="xl" fw="900">
+            PRODUCT{" "}
+            <Text span c="blue" inherit>
+              {params.id}
+            </Text>
+          </Text>
+          <Text>Category: {category}</Text>
+          <Space h="xl" />
+          <Space h="xl" />
+          <Text>{desc}</Text>
+          <Space h="xl" />
+          <Text>Price: ${price}</Text>
+          <Text style={{ display: "flex" }}>
+            Rating: {rating}
+            <Rating defaultValue={2} color="gray" size="xs" count={1} />
+          </Text>
         </Box>
       </Box>
     </>
