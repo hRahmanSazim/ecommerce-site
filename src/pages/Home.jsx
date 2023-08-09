0a2579092b0a25e43e8b4aeb7b6a78d5f7179506
Hasibur Rahman <hasibur.rahman@sazim.io>
2023-08-09 20:13:17 +0600

    add homepage and product page sync

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,80 +1,107 @@
-import { Center, SimpleGrid, Image, Text, Button, Box } from "@mantine/core";
-import { useState } from "react";
+import {
+  Center,
+  SimpleGrid,
+  Image,
+  Button,
+  Box,
+  Text,
+  Flex,
+} from "@mantine/core";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const Home = () => {
-  const [image, setImage] = useState("https://i.ibb.co/4dTYNvQ/Image-1.png");
-  // const [title, setTitle] = useState("product description");
+  const [image1, setImage1] = useState("");
+  const [image2, setImage2] = useState("");
+  const [image3, setImage3] = useState("");
+  const [image4, setImage4] = useState("");
+  const [image5, setImage5] = useState("");
+  const [image6, setImage6] = useState("");
+  const [image7, setImage7] = useState("");
+  const [image8, setImage8] = useState("");
+  const [items, setItems] = useState([]);
+  const [products, setProducts] = useState("");
 
-  // const handleClick = (e) => {
-  //   const givenId = e.currentTarget.id;
-  //   console.log(givenId);
-  // };
+  const nums = [];
+  useEffect(() => {
+    const arr = new Set();
+    while (arr.size !== 8) {
+      arr.add(Math.floor(Math.random() * 30) + 1);
+    }
+    for (const num of arr.values()) {
+      nums.push(num);
+    }
+    setItems(nums);
 
-  // async function fetchData(e, id) {
-  //   // const id = e.target.id;
-  //   // console.log(e.target);
-  //   // console.log(e.target.id);
-  //   const response = await fetch(`https://dummyjson.com/products/${id}`);
-  //   const data = await response.json();
-  //   console.log(data.products[2]);
-  // }
+    async function fetchData() {
+      const res = await fetch("https://dummyjson.com/products/");
+      const data = await res.json();
+      setProducts(data);
+      setImage1(data.products[nums[0] - 1].images[1]);
+      setImage2(data.products[nums[1] - 1].images[1]);
+      setImage3(data.products[nums[2] - 1].images[1]);
+      setImage4(data.products[nums[3] - 1].images[1]);
+      setImage5(data.products[nums[4] - 1].images[1]);
+      setImage6(data.products[nums[5] - 1].images[1]);
+      setImage7(data.products[nums[6] - 1].images[1]);
+      setImage8(data.products[nums[7] - 1].images[1]);
+    }
+    fetchData();
+  }, []);
 
   return (
     <SimpleGrid cols={4} spacing="xl" verticalSpacing="xl">
       <Box className="first" id="1">
-        <Image maw={240} mx="auto" radius="md" src={image} alt="Product 1" />
-        <Center>
-          <Link to="/product/1">Go To Product!! </Link>
-          <Button>Buy!</Button>
-        </Center>
+        <Image maw={240} mx="auto" radius="md" src={image1} alt="Product 1" />
+        <Link to={`/product/${items[0]}`}>Go To Product!! </Link>
+        <Button> Buy!</Button>
       </Box>
       <Box id="2">
-        <Image maw={240} mx="auto" radius="md" src={image} alt="Product 2" />
+        <Image maw={240} mx="auto" radius="md" src={image2} alt="Product 2" />
         <Center>
-          <Link to="/product/2">Go To Product!! </Link>
+          <Link to={`/product/${items[1]}`}>Go To Product!! </Link>
           <Button>Buy!</Button>
         </Center>
       </Box>
       <Box id="3">
-        <Image maw={240} mx="auto" radius="md" src={image} alt="Product 3" />
+        <Image maw={240} mx="auto" radius="md" src={image3} alt="Product 3" />
         <Center>
-          <Link to="/product/3">Go To Product!! </Link>
+          <Link to={`/product/${items[2]}`}>Go To Product!! </Link>
           <Button>Buy!</Button>
         </Center>
       </Box>
       <Box id="4">
-        <Image maw={240} mx="auto" radius="md" src={image} alt="Product 4" />
+        <Image maw={240} mx="auto" radius="md" src={image4} alt="Product 4" />
         <Center>
-          <Link to="/product/4">Go To Product!! </Link>
+          <Link to={`/product/${items[3]}`}>Go To Product!! </Link>
           <Button>Buy!</Button>
         </Center>
       </Box>
       <Box id="5">
-        <Image maw={240} mx="auto" radius="md" src={image} alt="Product 5" />
+        <Image maw={240} mx="auto" radius="md" src={image5} alt="Product 5" />
         <Center>
-          <Link to="/product/5">Go To Product!! </Link>
+          <Link to={`/product/${items[4]}`}>Go To Product!! </Link>
           <Button>Buy!</Button>
         </Center>
       </Box>
       <Box id="6">
-        <Image maw={240} mx="auto" radius="md" src={image} alt="Product 6" />
+        <Image maw={240} mx="auto" radius="md" src={image6} alt="Product 6" />
         <Center>
-          <Link to="/product/6">Go To Product!! </Link>
+          <Link to={`/product/${items[5]}`}>Go To Product!! </Link>
           <Button>Buy!</Button>
         </Center>
       </Box>
       <Box id="7">
-        <Image maw={240} mx="auto" radius="md" src={image} alt="Product 7" />
+        <Image maw={240} mx="auto" radius="md" src={image7} alt="Product 7" />
         <Center>
-          <Link to="/product/7">Go To Product!! </Link>
+          <Link to={`/product/${items[6]}`}>Go To Product!! </Link>
           <Button>Buy!</Button>
         </Center>
       </Box>
       <Box id="8">
-        <Image maw={240} mx="auto" radius="md" src={image} alt="Product 8" />
+        <Image maw={240} mx="auto" radius="md" src={image8} alt="Product 8" />
         <Center>
-          <Link to="/product/8">Go To Product!! </Link>
+          <Link to={`/product/${items[7]}`}>Go To Product!! </Link>
           <Button>Buy!</Button>
         </Center>
       </Box>
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,4 +1,3 @@
-import Header from "../components/Header";
 import "./product.css";
 import { useParams } from "react-router-dom";
 import {
@@ -19,67 +18,50 @@ const Product = () => {
   const [category, setCategory] = useState("");
   const [price, setPrice] = useState("");
   const [rating, setRating] = useState("");
+  const [title, setTitle] = useState("");
 
   useEffect(() => {
     let data;
     async function fetchData() {
       const res = await fetch(`https://dummyjson.com/products/${params.id}`);
       data = await res.json();
-      // console.log(data.thumbnail);
-      // console.log(data);
+
       setImage(data.thumbnail);
       setDesc(data.description);
       setCategory(data.category);
       setPrice(data.price);
       setRating(data.rating);
+      setTitle(data.title);
     }
     fetchData().catch(console.error);
-    // fetch(`https://dummyjson.com/products/${params.id}`)
-    //   .then((res) => res.json())
-    //   .then((obj) => {
-    //     data = obj;
-    //   })
-    //   .then(console.log(data));
-    // // setImage(data.images[1]);
   }, [params.id]);
 
   return (
-    <>
-      <Box className="product-page">
-        <Container py={45}>
-          <Box className="product-image">
-            <Center maw={400} mx="auto">
-              <Image
-                maw={400}
-                mx="auto"
-                radius="md"
-                src={image}
-                alt="Product"
-              />
-            </Center>
-          </Box>
-        </Container>
-        <Box className="product-info">
-          <Text fz="xl" fw="900">
-            PRODUCT{" "}
-            <Text span c="blue" inherit>
-              {params.id}
-            </Text>
-          </Text>
-          <Space h="sm" />
-          <Text>Category: {category}</Text>
-          <Space h="xl" />
-          {/* <Space h="xl" /> */}
-          <Text>{desc}</Text>
-          <Space h="xl" />
-          <Text>Price: ${price}</Text>
-          <Text style={{ display: "flex" }}>
-            Rating: {rating}
-            <Rating defaultValue={2} color="yellow" size="sm" count={1} />
-          </Text>
+    <Box className="product-page">
+      <Container py={45}>
+        <Box className="product-image">
+          <Center maw={400} mx="auto">
+            <Image maw={400} mx="auto" radius="md" src={image} alt="Product" />
+          </Center>
         </Box>
+      </Container>
+      <Box className="product-info">
+        <Text fz="xl" fw="900">
+          {title}
+        </Text>
+        <Space h="sm" />
+        <Text>Category: {category}</Text>
+        <Space h="xl" />
+        {/* <Space h="xl" /> */}
+        <Text>{desc}</Text>
+        <Space h="xl" />
+        <Text>Price: ${price}</Text>
+        <Text style={{ display: "flex" }}>
+          Rating: {rating}
+          <Rating defaultValue={2} color="yellow" size="sm" count={1} />
+        </Text>
       </Box>
-    </>
+    </Box>
   );
 };
 
