e6aceb296359dc347ec9a8b93d62904f2a46ce72
Hasibur Rahman <hasibur.rahman@sazim.io>
2023-08-11 20:17:39 +0600

    update to Product component

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -13,53 +13,49 @@ import { useEffect, useState } from "react";
 
 const Product = () => {
   const params = useParams();
-  const [image, setImage] = useState("");
-  const [desc, setDesc] = useState("");
-  const [category, setCategory] = useState("");
-  const [price, setPrice] = useState("");
-  const [rating, setRating] = useState("");
-  const [title, setTitle] = useState("");
+  const [product, setProduct] = useState();
 
   useEffect(() => {
-    let data;
     async function fetchData() {
       const res = await fetch(`https://dummyjson.com/products/${params.id}`);
-      data = await res.json();
-      setImage(data.thumbnail);
-      setDesc(data.description);
-      setCategory(data.category);
-      setPrice(data.price);
-      setRating(data.rating);
-      setTitle(data.title);
+      const data = await res.json();
+      setProduct(data);
     }
     fetchData().catch(console.error);
   }, [params.id]);
-
   return (
-    <Box className="product-page">
-      <Container py={45}>
-        <Box className="product-image">
-          <Center maw={400} mx="auto">
-            <Image maw={400} mx="auto" radius="md" src={image} alt="Product" />
-          </Center>
+    product && (
+      <Box className="product-page">
+        <Container py={45}>
+          <Box className="product-image">
+            <Center maw={400} mx="auto">
+              <Image
+                maw={400}
+                mx="auto"
+                radius="md"
+                src={product.thumbnail}
+                alt="Product"
+              />
+            </Center>
+          </Box>
+        </Container>
+        <Box>
+          <Text fz="xl" fw="900">
+            {product.title}
+          </Text>
+          <Space h="sm" />
+          <Text>Category: {product.category}</Text>
+          <Space h="xl" />
+          <Text>{product.description}</Text>
+          {/* <Space h="xl" /> */}
+          <Text lineClamp={1} display="flex">
+            Price: ${product.price} <Space w="xl" />
+            Rating: {product.rating}
+            <Rating defaultValue={2} color="yellow" size="sm" count={1} />
+          </Text>
         </Box>
-      </Container>
-      <Box>
-        <Text fz="xl" fw="900">
-          {title}
-        </Text>
-        <Space h="sm" />
-        <Text>Category: {category}</Text>
-        <Space h="xl" />
-        <Text>{desc}</Text>
-        {/* <Space h="xl" /> */}
-        <Text lineClamp={1} display="flex">
-          Price: ${price} <Space w="xl" />
-          Rating: {rating}
-          <Rating defaultValue={2} color="yellow" size="sm" count={1} />
-        </Text>
       </Box>
-    </Box>
+    )
   );
 };
 
